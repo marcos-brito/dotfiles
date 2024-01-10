6127787844d810420d6e123e86bedda82bc20a48
Marcos Brito <marcos.brito0507@gmail.com>
2024-01-10 19:24:15 -0300

    add new install script (never tested)

---
+++ install.sh
@@ -0,0 +1,34 @@
+!#/bin/bash
+
+IGNORE=(.git .gitignore install.py install_nvim_on_windows.py install.sh README.md)
+INSTALLTION_DIR=.config
+
+backup() {
+	mkdir -p "$INSTALLTION_DIR"/old
+	cp -r "$1"/* "$INSTALLTION_DIR"/old/"$1"
+}
+
+install_with_symlinks() {
+	for file in $(ls); do
+		if [[ ! " ${IGNORE[@]} " =~ " $file " ]]; then
+			backup "$file"
+			ln -s $(realpath "$file") "$INSTALLTION_DIR"/"$file"
+		fi
+	done
+}
+
+install_with_cp() {
+	for file in $(ls); do
+		if [[ ! " ${IGNORE[@]} " =~ " $file " ]]; then
+			backup "$file"
+			cp -r $(realpath "$file") "$INSTALLTION_DIR"/"$file"
+		fi
+	done
+}
+
+read -p "Should we use symlinks? (Y/N): " choice
+if [ -z "$choice" || "$choice" == "Y" || "$choice" == "y" ]; then
+	install_with_symlinks
+else
+	install_with_cp
+fi
